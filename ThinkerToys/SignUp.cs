46afdcfa999f2edcfbb54bfbab07d43f1024a03a
MahmoodAbuGneam <mahmoodpsgn@gmail.com>
2024-06-30 15:47:24 +0300

    Add project files.

---
+++ ThinkerToys/SignUp.cs
@@ -0,0 +1,15 @@
+namespace ThinkerToys
+{
+    public partial class SignUp : Form
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
