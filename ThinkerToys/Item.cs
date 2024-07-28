b20c521c700ff9f693b02d73f39dc1c7ab88d7ab
omarbridgetech <38240168+omarbridgetech@users.noreply.github.com>
2024-07-28 23:29:18 +0300

    Complete class Item

--- ThinkerToys/Item.cs
+++ ThinkerToys/Item.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ThinkerToys
 {
-    internal class Item
+    public class Item
     {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public Bitmap Image { get; set; }
+        public EventHandler BuyEventHandler { get; set; }
+
+        public Item(string name, int price, string category, Bitmap image, EventHandler buyEventHandler)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            Image = image;
+            BuyEventHandler = buyEventHandler;
+        }
     }
 }
